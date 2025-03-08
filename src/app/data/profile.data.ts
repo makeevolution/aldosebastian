6aa9781b3b1998ed0c03465347690c254af750c1
aldo <aldo_hasibuan@yahoo.com>
2025-03-08 16:34:34 +0100

    first commit

---
+++ src/app/data/profile.data.ts
@@ -0,0 +1,12 @@
+import { ProfileSchema } from "@data/schema/profile.schema";
+
+const profileData: ProfileSchema = {
+  name: 'Aldo Sebastian',
+  bio: 'Enthusiastic backend Software Engineer',
+  resumeLink: `cv.pdf`,
+  greetings: [
+    'Goededag',
+    'Hi there'
+  ],
+}
+export default profileData
